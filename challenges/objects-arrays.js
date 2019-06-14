eaf5b1b9a67cbd800b9083c6bfda6e78ebe4f2db
Lou <antilou86@gmail.com>
2019-06-14 12:37:54 -0400

    WIP: discovered issue with an array function, seeking help

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -64,10 +64,16 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
+/* Request 1: Create a new array called universities that contains all the universities in the graduates array. */ 
+const getColleges = (array) => {
+let newArray = array.map(graduate => graduate.university)
+//console.log(newArray);
+return newArray;
+}
+getColleges(graduates);
 
-Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+/* Once you have the new array created, sort the universities alphabetically and log the result. */
+const universities = getColleges(graduates).sort();
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -76,12 +82,28 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = [];
+const getContact = (array) => {
+  let newArray = array.map(graduate => graduate.first_name + ': ' + graduate.email);
+  return newArray;
+}
+const contactInfo = getContact(graduates);
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const getUni = (array) => {
+  let newArray=[];
+  array.forEach(
+    function(graduate){
+      if (graduate.university.indexOf('Uni') != -1) {
+      newArray.push(graduate.university);
+      }
+    //console.log(newArray);
+    return newArray;
+    }
+  )
+} 
+const uni = getUni(graduates);
 console.log(uni);
 
 
